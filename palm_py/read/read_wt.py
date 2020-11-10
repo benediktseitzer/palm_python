1516352c1a727879a92fb55703145d34c1ea7bb5
u232118 <benedikt.seitzer@uni-hamburg.de>
2020-11-10 11:40:26 +0100

    changed read of wt-data

--- palm_py/read/read_wt.py
+++ palm_py/read/read_wt.py
@@ -47,13 +47,21 @@ def read_wt_ver_pr(wt_file):
     read wind tunnel profile-data
     """
 
-    df = pd.read_table(wt_file,delimiter=',',header=1, usecols=[3,4,5])
+    df = pd.read_table(wt_file, delimiter=' ', header=7, usecols=[3,5,8])
     
     u_ref_dat = df.iloc[:,1].to_numpy()
     
-    u_ref = np.mean(u_ref_dat)
+    # u_ref_dat = np.mean(u_ref_dat)
 
     u = df.iloc[:,2].to_numpy()
     z = df.iloc[:,0].to_numpy()
     
-    return u, u_ref, z
+    return u, u_ref_dat, z
+
+def from_file(wt_file):
+    """ Create Timeseries object from file."""
+
+    z, u = np.genfromtxt(wt_file,usecols=(3,8),
+                                            skip_header=6,unpack=True)
+
+    return u, z
