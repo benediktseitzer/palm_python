808347e5ab168d18327753adc409c16e6cd29e35
u232118 <benedikt.seitzer@uni-hamburg.de>
2020-12-09 11:08:45 +0100

    changed terminal-output

--- palm_py/calc/calc_modelinput.py
+++ palm_py/calc/calc_modelinput.py
@@ -39,17 +39,14 @@ def calc_input_profile(m_u, m_z, z):
     u_prandtl: float
     u_fric: float
     """
-
-    print(papy.globals.z0)
-
     # calculate friction velocity
     ident = 17
     u_fric = (papy.globals.ka * m_u[ident]) / (np.log(m_z[ident]/papy.globals.z0))
-
     # calculate theoretical profiles
     u_prandtl = u_fric/papy.globals.ka * np.log((z[1:]-papy.globals.d0)/papy.globals.z0)
-
     # no-slip bc:
     u_prandtl = np.insert(u_prandtl,0,0.)
+    print('     z0 = {}'.format(papy.globals.z0))
+    print('     u* = {}'.format(u_fric))
 
     return u_prandtl, u_fric
