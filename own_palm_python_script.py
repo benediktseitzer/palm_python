b8e1e676ec743b01c143c87bbbe461b08ba7109d
Benedikt Seitzer <benedikt.seitzer@uni-hamburg.de>
2021-03-29 13:53:56 +0200

    minor script change

--- own_palm_python_script.py
+++ own_palm_python_script.py
@@ -78,8 +78,8 @@ GLOBAL VARIABLES
 """
 ################
 # PALM input files
-papy.globals.run_name = 'BA_BL_UW_001'
-papy.globals.run_number = '.026'
+papy.globals.run_name = 'BA_BL_UW_001_cyclic'
+papy.globals.run_number = '.003'
 papy.globals.run_numbers = ['.014', '.019']
 nc_file_grid = '{}_pr{}.nc'.format(papy.globals.run_name,papy.globals.run_number)
 nc_file_path = '../palm/current_version/JOBS/{}/OUTPUT/'.format(papy.globals.run_name)
