a494967a2b410c732ea088398df19e0f647d2b7c
Benedikt Seitzer <benedikt.seitzer@uni-hamburg.de>
2021-04-01 11:26:21 +0200

    function cleanup

--- palm_py/calc/calc.py
+++ palm_py/calc/calc.py
@@ -53,13 +53,10 @@ def calc_spectra(phi,t_eq,height,u_mean):
     """
     # sample frequency 
     freq = np.fft.fftfreq(np.size(phi),t_eq[1]-t_eq[0])
-
     # Fourier coefficients (normalized)
     fft_coeff = np.fft.fft(phi)/np.size(phi) 
-
     # Nyquist frequency
     freq_nyquist = np.abs(np.size(t_eq)//2)
-
     # spectral energy
     if np.size(phi)%2==0:
         E_uu = np.hstack((2. * np.abs(fft_coeff[0:freq_nyquist])**2., 
@@ -71,10 +68,8 @@ def calc_spectra(phi,t_eq,height,u_mean):
 
     # dimensionless S_uu by sigma and frequency
     S_uu = np.abs(freq[0:freq_nyquist+1])*S_uu/(np.nanstd(phi)**2.)
-
     # reduced frequency
     freq_red = freq*height/u_mean 
-
     smooth_spectra = True
     # spectral smoothing 
     if smooth_spectra:
@@ -85,7 +80,6 @@ def calc_spectra(phi,t_eq,height,u_mean):
                                     freq_sm.min(),freq_sm.max()+10**(-5),0.05))
         freq_sm = np.zeros(valcount.shape)
         S_uu_sm = np.zeros(valcount.shape)
-
         vc = 0  # counting values
         for i,n in enumerate(valcount):
             if n>0:
@@ -100,11 +94,9 @@ def calc_spectra(phi,t_eq,height,u_mean):
     index_sort = np.argsort(freq_sm)
     freq_sm_sort = freq_sm[index_sort]
     S_uu_sort = S_uu_sm[index_sort]
-
     # aliasing
     u_aliasing = freq_sm_sort.size-9+np.hstack((np.where
                                                 (np.diff(S_uu_sort[-10:])>=0.)[0],[9]))[0]
-
     return freq_sm_sort, S_uu_sort, u_aliasing
 
 def calc_autocorr(timeseries, maxlags):
@@ -149,14 +141,12 @@ def calc_lux(dt, u_comp):
         raise Exception('Too few value to estimate Lux!')
 
     mask = np.where(~np.isnan(u_comp))
-
     u = u_comp[mask]
-
     initial_guess = 1000 / dt  # number of values for the length of the first autocorrelation
     if initial_guess >= len(u):
         initial_guess = int(len(u) / 2)
+    
     lag_eq = np.arange(1, np.size(u) + 1) * dt  # array of time lags
-
     for lag in range(int(initial_guess), len(lag_eq), int(initial_guess)):
         u_eq_acorr = papy.calc_autocorr(u, lag)  # autocorrelation (one sided) of time series u_eq
         if np.any(np.diff(u_eq_acorr) > 0):  # if a single value of the derivation autocorrelation is smaller than 0
@@ -164,24 +154,18 @@ def calc_lux(dt, u_comp):
             break
 
     lag_eq = lag_eq[:len(u_eq_acorr)]
-
     Lux = 0.
     # see dissertation R.Fischer (2011) for calculation method
     for i in range(np.size(u_eq_acorr) - 2):
-
         autc1 = u_eq_acorr[i]
-
         autc2 = u_eq_acorr[i + 1]
-
         Lux = Lux + (autc1 + autc2) * 0.5
-
         if autc2 > autc1:
             acorr_fit = np.polyfit(lag_eq[:i], np.log(abs(u_eq_acorr[:i])), deg=1)
             acorr_fit = np.exp(acorr_fit[0] * lag_eq + acorr_fit[1])
 
             if np.min(acorr_fit) < 0.001:
                 ix = np.where(acorr_fit < 0.001)[0][0]
-
             else:
                 ix = acorr_fit.size
 
@@ -221,7 +205,6 @@ def calc_turbint(u_comp, v_comp, w_comp):
     I_u = u_std/np.mean(M)
     I_v = v_std/np.mean(M)
     I_w = w_std/np.mean(M)
-
     # output array of function    
     data = np.array([I_u, I_v, I_w])
     
@@ -253,18 +236,13 @@ def calc_fluxes(u_comp, v_comp, w_comp):
     u_mean = np.mean(u_comp)
     v_mean = np.mean(v_comp)
     w_mean = np.mean(w_comp)
-
     u_prime = u_comp - u_mean
     v_prime = v_comp - v_mean    
     w_prime = w_comp - w_mean
-
     flux11 = np.mean(u_prime * u_prime)
-    u_std = np.std(u_comp)
-
     flux22 = np.std(v_comp)
     flux33 = np.std(w_comp)
     flux12 = np.mean(u_prime * v_prime)
     flux13 = np.mean(u_prime * w_prime)
     flux23 = np.mean(v_prime * w_prime)
-    
     return flux11, flux22, flux33, flux12, flux13, flux23
