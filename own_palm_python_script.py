f662bac76405daf46430265ae102494bcab5c845
Benedikt Seitzer <benedikt.seitzer@uni-hamburg.de>
2021-06-11 09:42:17 +0200

    minor script change

--- own_palm_python_script.py
+++ own_palm_python_script.py
@@ -79,7 +79,7 @@ GLOBAL VARIABLES
 ################
 # PALM input files
 papy.globals.run_name = 'BA_BL_UW_001'
-papy.globals.run_number = '.035'
+papy.globals.run_number = '.037'
 papy.globals.run_numbers = ['.025', '.026']
 nc_file_grid = '{}_pr{}.nc'.format(papy.globals.run_name,papy.globals.run_number)
 nc_file_path = '../palm/current_version/JOBS/{}/OUTPUT/'.format(papy.globals.run_name)
@@ -87,12 +87,12 @@ nc_file_path = '../palm/current_version/JOBS/{}/OUTPUT/'.format(papy.globals.run
 #                     'M10','M11', 'M12', 'M13', 'M14', 'M15', 'M16', 'M17', 'M18', 'M19', 'M20']
 # height_list = [2., 4., 5., 7.5, 10., 15.,  20., 25., 30., 35., 40., 45., 50., 60.,
 #                      70., 80., 90., 100., 125., 150.]
-# mask_name_list = ['M01', 'M02', 'M03', 'M04', 'M05', 'M06', 'M07', 'M08', 'M09', 
-#                     'M10','M11', 'M12', 'M13', 'M14', 'M15', 'M16', 'M17', 'M18', 'M19']
-# height_list = [2., 4., 5., 7.5, 10., 15.,  20., 25., 30., 35., 40., 45., 50., 60.,
-#                      70., 80., 90., 100., 125.]
-mask_name_list = ['M01', 'M02', 'M03', 'M04', 'M05', 'M06', 'M07', 'M08', 'M09']
-height_list = [2., 4., 5., 7.5, 10., 15.,  20., 25., 30.]
+mask_name_list = ['M01', 'M02', 'M03', 'M04', 'M05', 'M06', 'M07', 'M08', 'M09', 
+                    'M10','M11', 'M12', 'M13', 'M14', 'M15', 'M16', 'M17', 'M18', 'M19']
+height_list = [2., 4., 5., 7.5, 10., 15.,  20., 25., 30., 35., 40., 45., 50., 60.,
+                     70., 80., 90., 100., 125.]
+# mask_name_list = ['M01', 'M02', 'M03', 'M04', 'M05', 'M06', 'M07', 'M08', 'M09']
+# height_list = [2., 4., 5., 7.5, 10., 15.,  20., 25., 30.]
 
 
 
@@ -105,13 +105,13 @@ wt_file_ref = '{}/wtref/{}_wtref.txt'.format(wt_path, wt_filename)
 wt_scale = 100.
 
 # PHYSICS
-papy.globals.z0 = 0.06
+papy.globals.z0 = 0.0361
 papy.globals.alpha = 0.168
 papy.globals.ka = 0.41
 papy.globals.d0 = 0.
-papy.globals.nx = 127
-papy.globals.ny = 127
-papy.globals.dx = 2
+papy.globals.nx = 255
+papy.globals.ny = 255
+papy.globals.dx = 4
 
 # test-cases for spectral analysis testing
 test_case_list = ['frequency_peak']
@@ -130,11 +130,11 @@ mode = mode_list[1]
 # compute_simrange = False
 # compute_modelinput = True
 
-compute_lux = True
+compute_lux = False
 compute_timeseries = True
-compute_turbint = True
+compute_turbint = False
 compute_vertprof = True
-compute_spectra = True
+compute_spectra = False
 compute_crosssections = True
 compute_pure_fluxes = False
 compute_simrange = False
@@ -528,9 +528,9 @@ if compute_modelinput:
         wt_u_pr, wt_u_ref, wt_z = papy.read_wt_ver_pr(wt_file_pr, wt_file_ref, wt_scale)
         print('\n wind tunnel profile loaded \n')
         # calculate z
-        domain_upper = 32.
-        z = np.linspace(0., domain_upper, 17)
-        reference_height = [7., 70]
+        domain_upper = 128.
+        z = np.linspace(0., domain_upper, 33)
+        reference_height = [7., 50.]
 
         # calculate theoretical profile
         u_pr, u_fric = papy.calc_input_profile(wt_u_pr, wt_z, z, reference_height)
