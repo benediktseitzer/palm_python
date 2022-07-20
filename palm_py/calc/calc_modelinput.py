7c82c2f602985019470f2a5f0940d09f85d8245f
Benedikt Seitzer <benedikt.seitzer@uni-hamburg.de>
2022-07-20 18:03:41 +0200

    calc topo-files roughness and building

--- palm_py/calc/calc_modelinput.py
+++ palm_py/calc/calc_modelinput.py
@@ -13,6 +13,7 @@ IMPORTS
 ################
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 import palm_py as papy
 
@@ -25,8 +26,8 @@ FUNCTIONS
 __all__ = [
     'calc_input_profile',
     'calc_topofile_building',
-    'calc_topofile_roughness'
-]
+    'calc_topofile_roughness',
+    'calc_topofile_roughness_building']
 
 def calc_input_profile(m_u, m_z, z, ref_height):
     """
@@ -83,6 +84,12 @@ def calc_topofile_building(building_height, building_x_length, building_y_length
                 if j>((dx*ny/2.)-building_y_length/2.) and j<((dx*ny/2.)+building_y_length/2.):
                     topo_matrix[i,j] = building_height
                     # print(i, j, topo_matrix[i,j])
+    plt.figure(11)
+    plt.imshow(topo_matrix)
+    plt.xlabel('x in m')
+    plt.xlabel('y in m')    
+    plt.show()
+    plt.close(11)
     np.savetxt('{}_topo'.format(papy.globals.run_name), topo_matrix, fmt='%1.0f', delimiter=' ')
     print('     Saved topo-file to {}_topo'.format(papy.globals.run_name))
 
@@ -112,14 +119,71 @@ def calc_topofile_roughness(rough_dist_x, rough_dist_y, rough_height):
         if dx*i % rough_dist_x == 0:
             for j in range(ny):
                 if dx*j % rough_dist_y == 0:
-                    topo_matrix[i,j] = rough_height
-        elif (dx*i-15) % rough_dist_x == 0:
+                    topo_matrix[j,i] = rough_height
+        elif (dx*i+15) % rough_dist_x == 0:
             for j in range(ny):
                 if (dx*j-7) % rough_dist_y == 0:
-                    topo_matrix[i,j] = rough_height
-        elif (dx*i-30) % rough_dist_x == 0:
+                    topo_matrix[j,i] = rough_height
+        elif (dx*i+30) % rough_dist_x == 0:
             for j in range(ny):
                 if (dx*j-14) % rough_dist_y == 0:
-                    topo_matrix[i,j] = rough_height
+                    topo_matrix[j,i] = rough_height
+    plt.figure(11)
+    plt.imshow(topo_matrix)
+    plt.xlabel('x in m')
+    plt.xlabel('y in m')    
+    plt.show()
+    plt.close(11)
+    np.savetxt('{}_topo'.format(papy.globals.run_name), topo_matrix, fmt='%1.0f', delimiter=' ')
+    print('     Saved topo-file to {}_topo'.format(papy.globals.run_name))
+
+
+def calc_topofile_roughness_building(rough_dist_x, rough_dist_y, rough_height, building_height, building_x_length, building_y_length):
+    """
+    Construct simple topography ascii-file for wind tunnel roughness
+
+    -----------
+    Parameters
+    nx: nx
+    ny: ny
+    rough_dist_x: float
+    rough_dist_y: float
+    rough_height: float
+
+    ---------
+    Returns
+
+    """
+    nx = papy.globals.nx + 1
+    ny = papy.globals.ny + 1
+    dx = papy.globals.dx
+    topo_matrix = np.zeros((nx,ny))
+    # wind tunnel roughness elements
+    for i in range(nx):
+        if dx*i % rough_dist_x == 0:
+            for j in range(ny):
+                if dx*j % rough_dist_y == 0:
+                    topo_matrix[j,i] = rough_height
+        elif (dx*i+15) % rough_dist_x == 0:
+            for j in range(ny):
+                if (dx*j-7) % rough_dist_y == 0:
+                    topo_matrix[j,i] = rough_height
+        elif (dx*i+30) % rough_dist_x == 0:
+            for j in range(ny):
+                if (dx*j-14) % rough_dist_y == 0:
+                    topo_matrix[j,i] = rough_height
+    # building geometry
+    for i in range(nx):
+        if dx*i>((dx*nx/2.)-building_x_length/2.) and i<((dx*nx/2.)+building_x_length/2.):
+            for j in range(ny):
+                if j>((dx*ny/2.)-building_y_length/2.) and j<((dx*ny/2.)+building_y_length/2.):
+                    topo_matrix[i,j] = building_height
+
+    plt.figure(11)
+    plt.imshow(topo_matrix)
+    plt.xlabel('x in m')
+    plt.xlabel('y in m')    
+    plt.show()
+    plt.close(11)
     np.savetxt('{}_topo'.format(papy.globals.run_name), topo_matrix, fmt='%1.0f', delimiter=' ')
     print('     Saved topo-file to {}_topo'.format(papy.globals.run_name))
