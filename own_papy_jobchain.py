3b0da39f8a25f9b5766378a4c4df73b4f2d9e869
Benedikt Seitzer <benedikt.seitzer@uni-hamburg.de>
2022-09-27 17:43:06 +0200

    minor changes

--- own_papy_jobchain.py
+++ own_papy_jobchain.py
@@ -34,15 +34,17 @@ GLOBAL VARIABLES
 """
 ################
 # PALM input files
-papy.globals.run_name = 'SB_SI_back'
-papy.globals.run_number = '.030'
+papy.globals.run_name = 'SB_SI_BL'
+papy.globals.run_number = '.045'
 # papy.globals.run_numbers = ['.029', '.028']
-papy.globals.jobchain_numbers = ['.001', '.002', '.003', '.004', '.005',
-                                '.006', '.007', '.008', '.009', '.010', '.011', 
-                                '.012', '.013', '.014', '.015', '.016', '.017', 
-                                '.018', '.019', '.020', '.021', '.022', '.023', 
-                                '.024', '.025', '.026', '.027', '.028', '.029',
-                                '.030']
+papy.globals.jobchain_numbers = ['.001', '.002', '.003', '.004', '.005', '.006',
+                                '.007', '.008', '.009', '.010', '.011', '.012', 
+                                '.013', '.014', '.015', '.016', '.017', '.018',
+                                '.019', '.020', '.021', '.022', '.023', '.024',
+                                '.025', '.026', '.027', '.028', '.029', '.030', 
+                                '.031', '.032', '.033', '.034', '.035', '.036',
+                                '.037', '.038', '.039', '.040', '.041', '.042',
+                                '.043', '.044', '.045']
 nc_file_grid = '{}_pr{}.nc'.format(papy.globals.run_name,papy.globals.run_number)
 nc_file_path = '../palm/current_version/JOBS/{}/OUTPUT/'.format(papy.globals.run_name)
 
